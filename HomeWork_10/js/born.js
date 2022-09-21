3623599832a69487508ad3ad3c22a670b2ab5333
Bohdan <bogdandnes1989@gmail.com>
2022-09-21 15:54:36 +0300

    second part

--- HomeWork_10/js/born.js
+++ HomeWork_10/js/born.js
@@ -4,13 +4,28 @@ const codeUser = document.querySelector('.homework__input-code');
 const submit = document.querySelector('.homework__button');
 
 const REG_PASSPORT = /[a-zA-z\d]/;
+const REG_NUM = /\d/;
 
 
-submit.addEventListener('click', check());
-function check() {
-    if (passportUser.value == REG_PASSPORT) {
-        console.log('Correct');
-    }
-}
+submit.onclick = function (e) {
+    e.preventDefault();
+    if (!REG_PASSPORT.test(passportUser.value) || passportUser.value.length != 8) {
+        passportUser.nextElementSibling.innerHTML = 'Error';
+    } else {
+        passportUser.nextElementSibling.innerHTML = 'Correct';
+    };
+
+    if (!REG_NUM.test(phoneUser.value) || phoneUser.value.length != 12) {
+        phoneUser.nextElementSibling.innerHTML = 'Error';
+    } else {
+        phoneUser.nextElementSibling.innerHTML = 'Correct';
+    };
+
+    if (!REG_NUM.test(codeUser.value) || codeUser.value.length != 10) {
+        codeUser.nextElementSibling.innerHTML = 'Error';
+    } else {
+        codeUser.nextElementSibling.innerHTML = 'Correct';
+    };
+};
 
 
